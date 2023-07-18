b0a3133ac014dce16cd9b0ca9c309b45ba53b9d1
bswaby <57374783+bswaby@users.noreply.github.com>
2023-07-18 16:18:56 -0500

    Update MM-PaymentNotify.py
    Moved program director to a global setting

--- Python Scripts/Member Manager Scripts/MM-PaymentNotify.py
+++ Python Scripts/Member Manager Scripts/MM-PaymentNotify.py
@@ -1,7 +1,16 @@
+#Automation Acccount Where Global Values are Stored
+AutomationAccount = 40678
 
 ProgramID = model.Data.ProgramID
 ProgramName = model.Data.ProgramName
 
+#Get Email from and EmailAddress
+EmailFrom = model.ExtraValueInt(AutomationAccount, str(ProgramID) + '_EmailFrom')
+getEmailAddress = q.QuerySql("SELECT TOP 1 EmailAddress from People Where PeopleId = " + str(EmailFrom))
+for em in getEmailAddress:
+        Data.email = em.EmailAddress
+        
+#Build PayLink
 paylink = " "
 #due = float(model.Data.totaldue)
 due = '${:,.2f}'.format(float(model.Data.totaldue))
@@ -14,18 +23,17 @@ sendGroup = q.QuerySqlInt("SELECT TOP 1 ID from SmsGroups")
 message = '''Click to pay the outstanding balance of {0}.  {1}'''.format(due,paylink) 
 
 if model.Data.sendemail == "y":
-
-    if ProgramID == 1108:
-      model.Email(a.PeopleId, 3414, "dmeyer@fbchtn.org", "David Meyer - FBCHville", "FBCHville Receipt", message)
-    elif ProgramID == 1109:
-      model.Email(a.PeopleId, 7365, "tklapwyk@fbchtn.org", "Tammy Klapwyk - FBCHville", "FBCHville Receipt", message)
-    elif ProgramID == 1143:
-      model.Email(a.PeopleId, 11180, "lpoteet@fbchtn.org", "Laura Poteet - FBCHville", "FBCHville Receipt", message)
-    elif ProgramID == 1149:
-      model.Email(a.PeopleId, 36153, "sgilmore@fbchtn.org", "Shannon Gilmore - FBCHville", "FBCHville Receipt", message)
-    elif ProgramID == 1152:
-      model.Email(a.PeopleId, 14221, "tbeals@fbchtn.org", "Tucker Beals - FBCHville", "FBCHville Test Receipt", message)
-
+    model.Email(a.PeopleId, EmailFrom, Data.email, "need to automate program name - FBCHville", "FBCHville Open Invoice", message)
+    #if ProgramID == 1108:
+    #  model.Email(a.PeopleId, 3414, "dmeyer@fbchtn.org", "David Meyer - FBCHville", "FBCHville Receipt", message)
+    #elif ProgramID == 1109:
+    #  model.Email(a.PeopleId, 7365, "tklapwyk@fbchtn.org", "Tammy Klapwyk - FBCHville", "FBCHville Receipt", message)
+    #elif ProgramID == 1143:
+    #  model.Email(a.PeopleId, 11180, "lpoteet@fbchtn.org", "Laura Poteet - FBCHville", "FBCHville Receipt", message)
+    #elif ProgramID == 1149:
+    #  model.Email(a.PeopleId, 36153, "sgilmore@fbchtn.org", "Shannon Gilmore - FBCHville", "FBCHville Receipt", message)
+    #elif ProgramID == 1152:
+    #  model.Email(a.PeopleId, 14221, "tbeals@fbchtn.org", "Tucker Beals - FBCHville", "FBCHville Test Receipt", message)
     print '''<h2>Paylink link to email<h2></br>'''
 else: 
     print '''<h2>Account does not have an email address associated with it</h2>'''
