be91261dc1a0bc00fe70aa2dfd4a18492905bcd3
Tucker <tuckerebeals@me.com>
2023-07-20 22:39:14 -0500

    Added basic cost/discount logic and multiple sub groups display for people shown

--- Python Scripts/Sandbox.py
+++ Python Scripts/Sandbox.py
@@ -1,9 +1,67 @@
-programNameQuery =  '''
-SELECT Name
-FROM Program
-WHERE Name = 'FMC'
-'''
-for a in q.QuerySql(programNameQuery):
-    programName = a.Name
-    
-print programName
+chargeVariables = {
+    'fees' : [{
+        'id': "1152_2-Day",
+        'name': "2-Day",
+        'frequency': "Monthly",
+        'description': "2-Day a Week Fee",
+        'cost': 200
+    },
+    {
+        'id': "1152_3-Day",
+        'name': "3-Day",
+        'frequency': "Monthly",
+        'description': "3-Day a Week Fee",
+        'cost': 300
+    },
+    {
+        'id': "1152_4-Day",
+        'name': "4-Day",
+        'frequency': "Monthly",
+        'description': "4-Day a Week Fee",
+        'cost': 400
+    }],
+    'discounts' : [
+        {
+            'title': "Staff Discount",
+            'percentage': 50,
+            'code': 1,
+        }
+        {
+            'title': "Multiple Family Member Discount",
+            'percentage': 10,
+            'code': 2,
+        }
+    ]
+}
+
+print(newChargeVar['fees'][0])
+
+
+
+
+
+
+
+
+[
+    {
+        'id': "1152_2-Day",
+        'name': "2-Day",
+        'frequency': "Monthly",
+        'description': "2-Day a Week Fee",
+        'cost': 200
+    },
+    {
+        'id': "1152_3-Day",
+        'name': "3-Day",
+        'frequency': "Monthly",
+        'description': "3-Day a Week Fee",
+        'cost': 300
+    },
+    {
+        'id': "1152_4-Day",
+        'name': "4-Day",
+        'frequency': "Monthly",
+        'description': "4-Day a Week Fee",
+        'cost': 400
+    }
