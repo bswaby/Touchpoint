d49a399da4d0b4a9de7b12f999d2162565b798fd
Ben Swaby <57374783+bswaby@users.noreply.github.com>
2026-05-10 11:57:53 -0500

    Revise header comments in TPxi_TransactionSearch.py
    Updated author information and added website details.

--- TPxi/Transaction Search/TPxi_TransactionSearch.py
+++ TPxi/Transaction Search/TPxi_TransactionSearch.py
@@ -1,13 +1,21 @@
 #Roles=Edit
-# Written By: Ben Swaby
-# Email: bswaby@fbchtn.org
-# GitHub:  https://github.com/bswaby/Touchpoint
-# ---------------------------------------------------------------
-# Support: These tools are free because they should be. If they've
-#          saved you time, consider DisplayCache - church digital
-#          signage that integrates with TouchPoint.
-#          https://displaycache.com
-# ---------------------------------------------------------------
+# Written By: Ben Swaby (TPxi Software, LLC)
+# Email: bswaby@fbchtn.org                                                                                                      
+# Website: https://tpxisoftware.com
+# GitHub: https://github.com/bswaby/Touchpoint  (50+ free tools)                                                                
+# ----------------------------------------------------------------                                                              
+# These tools are free because they should be.
+# If they've saved you time or helped your team, and you want to                                                                
+# support continued development, check out:                                                                                     
+#
+# DisplayCache(TM) - church digital signage that integrates with TouchPoint(R)                                                  
+# https://displaycache.com                                
+#
+# TPxi Go(TM) - your church contacts, wherever you work.
+# Look up anyone in TouchPoint(R), log calls and emails from Outlook                                                            
+# or your phone. No tab switching, no lost context.
+# https://tpxigo.com                                                                                                            
+# ----------------------------------------------------------------
 # Description: Search transactions by the last 4 digits of a
 #              credit card or ACH account number. Returns matching
 #              transactions with person, organization, payment
