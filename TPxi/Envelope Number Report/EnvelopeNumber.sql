1cc1dee2d0c82a41f684347873d83113bd0eddfa
Ben Swaby <57374783+bswaby@users.noreply.github.com>
2026-05-10 11:49:29 -0500

    Revise author details and add tool promotions
    Updated author information and added promotional content for TPxi tools.

--- TPxi/Envelope Number Report/EnvelopeNumber.sql
+++ TPxi/Envelope Number Report/EnvelopeNumber.sql
@@ -1,10 +1,23 @@
 -- ---------------------------------------------------------------
 -- Envelope Number Report
 -- ---------------------------------------------------------------
--- Written By: Ben Swaby
--- Email: bswaby@fbchtn.org
--- GitHub: https://github.com/bswaby/Touchpoint
--- ---------------------------------------------------------------
+-- Written By: Ben Swaby (TPxi Software, LLC)
+-- Email: bswaby@fbchtn.org                                                                                                      
+-- Website: https://tpxisoftware.com
+-- GitHub: https://github.com/bswaby/Touchpoint  (50+ free tools)                                                                
+-- ----------------------------------------------------------------                                                              
+-- These tools are free because they should be.
+-- If they've saved you time or helped your team, and you want to                                                                
+-- support continued development, check out:                                                                                     
+--
+-- DisplayCache(TM) - church digital signage that integrates with TouchPoint(R)                                                  
+-- https://displaycache.com                                
+--
+-- TPxi Go(TM) - your church contacts, wherever you work.
+-- Look up anyone in TouchPoint(R), log calls and emails from Outlook                                                            
+-- or your phone. No tab switching, no lost context.
+-- https://tpxigo.com                                                                                                            
+-- ----------------------------------------------------------------
 -- Description:
 --   Generates a mailing list of all envelope holders with their
 --   envelope number, name (formatted for joint/individual/deceased
